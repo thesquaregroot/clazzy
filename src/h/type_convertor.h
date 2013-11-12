2e8e5d490c71913b208f74a522285e93540c9942
Andrew Groot <groot.andrew@gmail.com>
2013-11-11 20:13:21 -0500

    Added more control in type_convertor for converting types, especially in regard to handling generics.  Also made the method signatures make a bit more sense by adding a new constructor to set defaults, rather than passing them into the methods).

--- src/h/type_convertor.h
+++ src/h/type_convertor.h
@@ -8,28 +8,37 @@
 namespace clazzy {
     class type_convertor {
         public:
-            void add_type(const std::string &clazzy_type, const std::string &lang_type, const std::string * const import_stmt = nullptr);
-            // converts with case as-is
-            std::string convert(const type_hint &, char generic_start = '<', char generic_end = '>') const;
-            // converts all type names to lower-case with underscores (like_this)
-            std::string convert_lc(const type_hint &, char generic_start = '<', char generic_end = '>') const;
-            // converts all type names to camel case (LikeThis)
-            std::string convert_cc(const type_hint &, char generic_start = '<', char generic_end = '>') const;
-
-            bool has_import(const std::string &) const;
-            std::vector<std::string> get_imports(const std::vector<type_hint> &) const;
-
-        private:
             enum case_conversion {
                 NONE,
                 LOWER,
                 CAMEL
             };
 
-            std::string convert_with_case(const type_hint &, char generic_start = '<', char generic_end = '>', case_conversion = NONE) const;
+            type_convertor() = default;
+            type_convertor(char gstart, char gend, case_conversion cc = NONE) : _generic_start(gstart),
+                                                                        _generic_end(gend),
+                                                                        _default_case(cc) {};
 
-            std::map<std::string,std::string> mappings;
-            std::map<std::string,std::string> imports;
+            void add_type(const std::string &clazzy_type, const std::string &lang_type, const std::string * const import_stmt = nullptr);
+            // converts with case as-is
+            std::string convert(const type_hint &) const;
+            // converts all type names to lower-case with underscores (like_this)
+            std::string convert_lc(const type_hint &) const;
+            // converts all type names to camel case (LikeThis)
+            std::string convert_cc(const type_hint &) const;
+
+            bool has_import(const std::string &) const;
+            std::vector<std::string> get_imports(const std::vector<type_hint> &) const;
+
+        private:
+            char _generic_start = '<';
+            char _generic_end = '>';
+            case_conversion _default_case = NONE;
+            
+            std::map<std::string,std::string> _mappings;
+            std::map<std::string,std::string> _imports;
+            
+            std::string convert_with_case(const type_hint &, const case_conversion) const;
     };
 }
 
